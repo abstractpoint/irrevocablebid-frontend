581446442e8af395168c01cff6b3f444ad506f69
Vanya A. Sergeev <v@sergeev.io>
2021-06-30 03:04:00 -0500

    seller: add bid info to approve guarantor modal

--- src/seller/modals/ApproveGuarantorModal.tsx
+++ src/seller/modals/ApproveGuarantorModal.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -10,10 +12,12 @@ import DialogActions from '@material-ui/core/DialogActions';
 import * as ethers from "ethers";
 
 import { BuyOrder } from "../../../lib";
-import { SellerEscrow } from "../../../lib";
-import { serializeSellOrder } from "../../../lib";
+import { SellerEscrow, SellerEscrowBidInfo } from "../../../lib";
+import { serializeSellOrder, serializeBuyOrder } from "../../../lib";
 
 import { EthereumContext, decodeBuyOrder } from "../../helpers";
+import { EthereumAddress } from "../../components/EthereumAddress";
+import { TokenAmount } from "../../components/TokenAmount";
 import { EthereumTransaction, EthereumTransactionStatus } from "../../components/EthereumTransaction";
 
 /******************************************************************************/
@@ -28,7 +32,8 @@ type ApproveGuarantorModalProps = {
 };
 
 type ApproveGuarantorModalState = {
-  buyOrderData: string;
+  buyOrder: BuyOrder | null;
+  bidInfo: SellerEscrowBidInfo | null;
 
   error?: string;
   transaction?: ethers.ContractTransaction;
@@ -37,27 +42,18 @@ type ApproveGuarantorModalState = {
 
 export class ApproveGuarantorModal extends React.Component<ApproveGuarantorModalProps, ApproveGuarantorModalState> {
   state: ApproveGuarantorModalState = {
-    buyOrderData: "",
+    buyOrder: null,
+    bidInfo: null,
     transactionStatus: EthereumTransactionStatus.None,
   };
 
   async handleClick() {
-    if (!this.props.context.deployment)
-      return;
-
-    let buyOrder: BuyOrder | null = null;
-    try {
-      buyOrder = decodeBuyOrder(this.props.context.deployment, this.props.sellerEscrow.sellOrder, this.state.buyOrderData);
-    } catch (err) {
-      alert(`Invalid buy order: ${err}`);
-    }
-
-    if (!buyOrder)
+    if (!this.props.context.deployment || !this.state.buyOrder)
       return;
 
     let transaction: ethers.ContractTransaction;
     try {
-      transaction = await this.props.sellerEscrow.commit(buyOrder);
+      transaction = await this.props.sellerEscrow.commit(this.state.buyOrder);
     } catch (err) {
       this.setState({...this.state, error: (err instanceof Object) ? `Error: ${err.message}` : err});
       return;
@@ -66,8 +62,22 @@ export class ApproveGuarantorModal extends React.Component<ApproveGuarantorModal
     this.setState({...this.state, transaction, error: undefined, transactionStatus: EthereumTransactionStatus.Pending});
   }
 
-  handleChange(value: string) {
-    this.setState({...this.state, buyOrderData: value});
+  async handleChange(value: string) {
+    if (!this.props.context.deployment || !this.props.context.provider)
+      return;
+
+    let buyOrder: BuyOrder | null = null;
+    try {
+      buyOrder = decodeBuyOrder(this.props.context.deployment, this.props.sellerEscrow.sellOrder, value);
+    } catch (err) {
+      this.setState({...this.state, error: (err instanceof Object) ? `Error: ${err.message}` : err});
+      return;
+    }
+
+    const sellerEscrow = new SellerEscrow(this.props.context.provider, this.props.context.deployment, this.props.sellerEscrow.sellOrder, buyOrder);
+    const bidInfo = await sellerEscrow.getBidInfo();
+
+    this.setState({...this.state, buyOrder, bidInfo});
   }
 
   handleComplete(success: boolean) {
@@ -78,16 +88,17 @@ export class ApproveGuarantorModal extends React.Component<ApproveGuarantorModal
   }
 
   redirectSeller() {
-    if (!this.props.sellerEscrow)
+    if (!this.props.sellerEscrow || !this.state.buyOrder)
       return;
 
     /* FIXME this is hacky to do in a modal */
 
     /* Serialize orders */
     const sellOrderData = serializeSellOrder(this.props.sellerEscrow.sellOrder);
+    const buyOrderData = serializeBuyOrder(this.state.buyOrder);
 
     /* Create updated URL */
-    const urlSearchParams = new URLSearchParams({sell: sellOrderData, buy: this.state.buyOrderData});
+    const urlSearchParams = new URLSearchParams({sell: sellOrderData, buy: buyOrderData});
     const sellerURL = window.location.origin + "/#/seller/?" + urlSearchParams.toString();
 
     /* Redirect to URL */
@@ -102,6 +113,12 @@ export class ApproveGuarantorModal extends React.Component<ApproveGuarantorModal
         <div>
           <DialogTitle id="simple-modal-title">Approve Guarantor</DialogTitle>
           <DialogContent>
+            {this.state.bidInfo &&
+              <List>
+                <ListItem>Guarantor: <EthereumAddress context={this.props.context} address={this.state.bidInfo.guarantorAddress} /></ListItem>
+                <ListItem>Escrow: <EthereumAddress context={this.props.context} address={this.state.bidInfo.escrowAddress} /></ListItem>
+                <ListItem>Price: <TokenAmount context={this.props.context} address={this.state.bidInfo.paymentTokenAddress} amount={this.state.bidInfo.price} /></ListItem>
+              </List>}
             <TextField label="Guarantor Bid" multiline fullWidth rows={4} variant="outlined"
                        onChange={(event: any) => { this.handleChange(event.target.value); }} />
             {this.state.error && <b>{this.state.error}</b>}
@@ -114,7 +131,8 @@ export class ApproveGuarantorModal extends React.Component<ApproveGuarantorModal
             </Button>
             <Button color="primary" variant="contained" onClick={() => { this.handleClick(); }}
                     disabled={this.state.transactionStatus == EthereumTransactionStatus.Pending ||
-                              this.state.transactionStatus == EthereumTransactionStatus.Success} autoFocus>
+                              this.state.transactionStatus == EthereumTransactionStatus.Success ||
+                              this.state.buyOrder === null} autoFocus>
               Approve
             </Button>
           </DialogActions>
